c96631418c6f57d31aca344553478be9569df80c
Carmendon24 <161263766+Carmendon24@users.noreply.github.com>
2024-02-26 01:17:05 -0500

    Update GlobalSiteHeader.java

--- src/main/java/pages/GlobalSiteHeader.java
+++ src/main/java/pages/GlobalSiteHeader.java
@@ -5,33 +5,33 @@ import org.openqa.selenium.WebDriver;
 
 public class GlobalSiteHeader {
 
-		WebDriver driver;
+	WebDriver driver;
 		
-		public GlobalSiteHeader(WebDriver driver) {
-			this.driver=driver;
-		}
+	public GlobalSiteHeader(WebDriver driver) {
+		this.driver=driver;
+	}
 	    
-		//Global Search Field Locator
-		By searchField = By.id("searchval");
+	//Global Search Field Locator
+	By searchField = By.id("searchval");
 		
-	    //View Cart Button Locator
-	    By cartButton = By.linkText("View Cart");
+	//View Cart Button Locator
+	By cartButton = By.linkText("View Cart");
 		
 
-		//Enter Keyword Search Term Method
-	    public void enterSearchTerm(String search) {
-	    		driver.findElement(searchField).sendKeys(search);
-		}
+	//Enter Keyword Search Term Method
+	public void enterSearchTerm(String search) {
+		driver.findElement(searchField).sendKeys(search);
+	}
 
-	    //Submit Keyword Search Method
-	    public void submitSearch() {
-	    		driver.findElement(searchField).submit();
-		}
-	    
-		//Select View Cart Method
-	    public void viewCart() {
+	//Submit Keyword Search Method
+	public void submitSearch() {
+		driver.findElement(searchField).submit();
+	}
+	   
+	//Select View Cart Method
+	public void viewCart() {
 	    	driver.findElement(cartButton).click();
-		}
+	}
 
 }
 
