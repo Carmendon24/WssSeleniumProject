6d3c4f9e380581a317c48b9f867f692ebd968e01
Carmendon24 <161263766+Carmendon24@users.noreply.github.com>
2024-02-26 01:12:46 -0500

    Update Cart.java

--- src/main/java/pages/Cart.java
+++ src/main/java/pages/Cart.java
@@ -5,26 +5,26 @@ import org.openqa.selenium.WebDriver;
 
 public class Cart {
 
-		WebDriver driver;
-		
-		public Cart(WebDriver driver) {
-			this.driver=driver;
-		}
-	    
-	    //Empty Cart Button Locator
-		By emptyCart = By.cssSelector(".emptyCartButton");
-		
-	    //Empty Cart Confirm Button Locator	    
-	    By emptyCartConfirm = By.cssSelector(".text-shadow-black-60");
-	    
-		//Select Empty Cart Method
-	    public void emptyCart() {
-	    	driver.findElement(emptyCart).click();
-		}
-
-	    //Select Confirm Empty Cart Method
-	    public void emptyCartConfirm() {
-	    	driver.findElement(emptyCartConfirm).click();
-		}
-	    
+	WebDriver driver;
+	
+	public Cart(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	//Empty Cart Button Locator
+	By emptyCart = By.cssSelector(".emptyCartButton");
+	
+	//Empty Cart Confirm Button Locator	    
+	By emptyCartConfirm = By.cssSelector(".text-shadow-black-60");
+	
+	//Select Empty Cart Method
+	public void emptyCart() {
+	driver.findElement(emptyCart).click();
+	}
+	
+	//Select Confirm Empty Cart Method
+	public void emptyCartConfirm() {
+	driver.findElement(emptyCartConfirm).click();
+	}
+    
 }
